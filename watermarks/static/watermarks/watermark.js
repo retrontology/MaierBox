f497350fdfc60455e026f2c7db87e7eaa5e5e8a8
Retrontology <retrontology@gmail.com>
2024-06-21 22:50:39 -0500

    kinda fix watermark preview

--- watermarks/static/watermarks/watermark.js
+++ watermarks/static/watermarks/watermark.js
@@ -1,14 +1,14 @@
 function update_watermark_preview(e) {
     text = document.getElementById("watermark_text").value;
     family = document.getElementById("watermark_font").selectedOptions[0].dataset['family'];
-    family = document.getElementById("watermark_font").selectedOptions[0].dataset['style'];
+    style = document.getElementById("watermark_font").selectedOptions[0].dataset['style'];
     size = document.getElementById("watermark_size").value*2;
     transparency = document.getElementById("watermark_transparency").value;
 
     watermark = document.getElementById("watermark");
     watermark.textContent = text;
     watermark.style.fontFamily = family;
-    watermark.style.fontStyle = 0;
+    watermark.style.fontStyle = style;
     watermark.style.fontSize = `${size}px`;
     watermark.style.opacity = (100-transparency)/100;
 }
