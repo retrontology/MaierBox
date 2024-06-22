1ab0d3cde488467cdc43005d9c74834c95514054
retrontology <retrontology@hotmail.com>
2024-06-22 07:11:45 -0500

    lock aspect ratios for preview

--- watermarks/static/watermarks/watermark.js
+++ watermarks/static/watermarks/watermark.js
@@ -1,3 +1,34 @@
+const COMMON_RATIOS = {
+    '1:1': {
+        'width': 400,
+        'height': 400,
+    },
+    '3:2': {
+        'width': 600,
+        'height': 400,
+    },
+    '4:3': {
+        'width': 600,
+        'height': 450,
+    },
+    '5:4': {
+        'width': 600,
+        'height': 480,
+    },
+    '7:5': {
+        'width': 600,
+        'height': 428,
+    },
+    '10:8': {
+        'width': 600,
+        'height': 480,
+    },
+    '11:8.5': {
+        'width': 600,
+        'height': 463,
+    },
+};
+
 function greatest_common_factor(a, b) {
     if (!b) {
         return a;
@@ -28,13 +59,37 @@ function update_watermark_preview() {
 }
 
 function on_preview_resize(entries, observer) {
-    let container = document.getElementById("watermark_container");
-    let width = container.offsetWidth;
-    let height = container.offsetHeight;
-    let factor = greatest_common_factor(width, height);
-    let size = document.getElementById("watermark_preview_size");
+    // let container = document.getElementById("watermark_container");
+    // let width = container.offsetWidth;
+    // let height = container.offsetHeight;
+    // let factor = greatest_common_factor(width, height);
+    // let size = document.getElementById("watermark_preview_size");
     let ratio = document.getElementById("watermark_preview_ratio");
-    size.textContent = `${width}×${height}`;
-    ratio.textContent = `${width/factor}:${height/factor}`;
+    // size.textContent = `${width}×${height}`;
+    // ratio.textContent = `${width/factor}:${height/factor}`;
+    ratio.textContent = document.getElementById("watermark_control_ratio").value;
     update_watermark_preview();
+}
+
+function on_ratio_changed(e) {
+    let ratio = document.getElementById("watermark_control_ratio").value;
+    let orientation = document.getElementById("watermark_control_orientation").value;
+    let preview = document.getElementById("watermark_preview");
+    if (orientation === "Landscape") {
+        preview.style.width = `${COMMON_RATIOS[ratio].width}px`;
+        preview.style.height = `${COMMON_RATIOS[ratio].height}px`;
+    }
+    else if (orientation === "Portrait") {
+        preview.style.width = `${COMMON_RATIOS[ratio].height}px`;
+        preview.style.height = `${COMMON_RATIOS[ratio].width}px`;
+    }
+}
+
+function populate_ratios(select) {
+    for (let ratio in COMMON_RATIOS) {
+        let option = document.createElement('option');
+        option.textContent = ratio;
+        select.appendChild(option)
+    }
+    on_ratio_changed(null);
 }
