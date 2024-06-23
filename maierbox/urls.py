fc942e5eff5507a79d9511ec590fbde86f438902
retrontology <retrontology@hotmail.com>
2024-06-23 01:58:04 -0500

    trim

--- maierbox/urls.py
+++ maierbox/urls.py
@@ -29,5 +29,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("accounts/", include("django.contrib.auth.urls")),
 ]
-if settings.DEBUG: # new
+if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
