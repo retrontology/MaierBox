ff7a4e4d7e77dc09c6b18c20913d16524ec1bad0
retrontology <retrontology@hotmail.com>
2024-07-14 00:39:06 -0500

    use keyfile

--- maierbox/settings.py
+++ maierbox/settings.py
@@ -19,13 +19,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1-_*npak+yg6(v2rcbe!p^3d#bg@o!y&zbu@&m*@-da+a(0$qw'
+with open(BASE_DIR / "secret.key") as f:
+    SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
