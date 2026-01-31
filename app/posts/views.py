6e17c94045c4c11a84d73d844cc486a584b1b83f
Cullen Knight <retrontology@hotmail.com>
2026-01-30 19:54:18 -0600

    add pagination

--- app/posts/views.py
+++ app/posts/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse, HttpResponseNotAllowed, HttpRequest, Http404
 from django.core.paginator import Paginator
+from django.utils.dateparse import parse_date
+from urllib.parse import urlencode
 from maierbox.util import JsonErrorResponse
 from .models import Post
 from images.models import WebImage
@@ -10,30 +12,79 @@ from markdown2 import markdown
 import uuid
 
 MAX_POSTS = 6
+MAX_PER_PAGE = 100
 
 def index(request: HttpRequest):
     
     if request.method != "GET":
         return HttpResponseNotAllowed(['GET'])
     
-    if 'page' in request.GET:
-        page = request.GET['page']
-    else:
-        page = 1
-    
-    if 'max' in request.GET:
-        max = request.GET['max']
-    else:
-        max = MAX_POSTS
+    page_number = request.GET.get('page', 1)
+    try:
+        page_number = int(page_number)
+    except (TypeError, ValueError):
+        page_number = 1
+
+    per_page = request.GET.get('max', MAX_POSTS)
+    try:
+        per_page = int(per_page)
+    except (TypeError, ValueError):
+        per_page = MAX_POSTS
+    if per_page < 1:
+        per_page = MAX_POSTS
+    if per_page > MAX_PER_PAGE:
+        per_page = MAX_PER_PAGE
+
+    date_from_raw = request.GET.get('start', '').strip()
+    date_to_raw = request.GET.get('end', '').strip()
+    date_from = parse_date(date_from_raw) if date_from_raw else None
+    date_to = parse_date(date_to_raw) if date_to_raw else None
+    if date_from and date_to and date_from > date_to:
+        date_from, date_to = date_to, date_from
+        date_from_raw, date_to_raw = date_to_raw, date_from_raw
+
+    sort = request.GET.get('sort', 'newest')
+    sort_options = {
+        'newest': '-date_created',
+        'oldest': 'date_created',
+    }
+    order_by = sort_options.get(sort, '-date_created')
+
+    posts = Post.objects.filter(published=True, unlisted=False)
+    if date_from:
+        posts = posts.filter(date_created__date__gte=date_from)
+    if date_to:
+        posts = posts.filter(date_created__date__lte=date_to)
 
     paginator = Paginator(
-        Post.objects.filter(published=True, unlisted=False).order_by("-date_created"),
-        per_page=max,
+        posts.order_by(order_by),
+        per_page=per_page,
     )
 
-    page = paginator.page(page)
+    page = paginator.get_page(page_number)
 
-    context = {'page': page}
+    query_params = {
+        'max': per_page,
+        'sort': sort if sort in sort_options else 'newest',
+    }
+    if date_from and date_from_raw:
+        query_params['start'] = date_from_raw
+    if date_to and date_to_raw:
+        query_params['end'] = date_to_raw
+    query_string = urlencode(query_params)
+    pagination_query = f"&{query_string}" if query_string else ""
+
+    context = {
+        'page': page,
+        'pagination_query': pagination_query,
+        'filters': {
+            'start': date_from_raw if date_from else '',
+            'end': date_to_raw if date_to else '',
+            'sort': sort if sort in sort_options else 'newest',
+            'max': per_page,
+        },
+        'per_page_options': [6, 12, 24, 48],
+    }
     return render(request, 'posts/index.html', context)
 
 def view(request: HttpRequest, id):
